c217755897ed58dcdf0803246beea095c79e5a09
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-05-31 18:09:00 +0200

    Fix non aligned training files after merge

--- nmtwizard/data.py
+++ nmtwizard/data.py
@@ -18,7 +18,7 @@ def merge_files_in_directory(input_dir, output_dir, src_suffix, tgt_suffix):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
-    src_files = [os.path.join(input_dir, f) for f in files if f.endswith(src_suffix)]
-    tgt_files = [os.path.join(input_dir, f) for f in files if f.endswith(tgt_suffix)]
+    src_files = sorted(os.path.join(input_dir, f) for f in files if f.endswith(src_suffix))
+    tgt_files = sorted(os.path.join(input_dir, f) for f in files if f.endswith(tgt_suffix))
     merge_files(src_files, os.path.join(output_dir, 'train.%s' % src_suffix))
     merge_files(tgt_files, os.path.join(output_dir, 'train.%s' % tgt_suffix))
