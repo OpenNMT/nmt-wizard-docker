f736c6ad9ab2e650e584dd0181f89f4cf0481837
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-08-10 16:37:38 +0200

    Update new Naver entrypoint

--- frameworks/naver_translate/entrypoint.py
+++ frameworks/naver_translate/entrypoint.py
@@ -63,8 +63,8 @@ class NaverTranslateFramework(Framework):
     def _preprocess_input(self, state, input):
         return input
 
-    def _postprocess_output(self, state, output):
-        return output
+    def _postprocess_output(self, state, source, target):
+        return target
 
 
 def translate_list(appid, secretKey, texts, source_language, target_language):
