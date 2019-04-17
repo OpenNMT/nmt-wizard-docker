e1a3f022c12be2ccb1650e0ee9ed73907bb89b07
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-04-17 10:06:45 +0200

    Keep directory structure when downloading files in config

--- nmtwizard/utility.py
+++ nmtwizard/utility.py
@@ -66,7 +66,9 @@ def resolve_remote_files(config, local_dir, storage_client):
         if not isinstance(value, six.string_types) or not storage_client.is_managed_path(value):
             return value
         storage_id, remote_path = storage_client.parse_managed_path(value)
-        local_path = os.path.join(local_dir, storage_id, os.path.basename(remote_path))
+        if remote_path[0] == '/':
+            remote_path = remote_path[1:]
+        local_path = os.path.join(local_dir, storage_id, remote_path)
         # can be a file or a directory
         storage_client.get(remote_path, local_path, storage_id=storage_id, directory=None)
         return local_path
