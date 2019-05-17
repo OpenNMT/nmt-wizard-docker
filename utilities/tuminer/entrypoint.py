8a4f85331ffd1dc04ad6af686323da4c91e84978
Jungi Kim <jungi.kim@systrangroup.com>
2019-05-17 10:18:24 -0700

    Honor --gpuid; Src,Tgt embedding in parallel in multi-GPU

--- utilities/tuminer/entrypoint.py
+++ utilities/tuminer/entrypoint.py
@@ -18,6 +18,128 @@ from nmtwizard.utility import resolve_environment_variables
 
 logger = get_logger(__name__)
 
+def setCUDA_VISIBLE_DEVICES(gpuid):
+    if gpuid is 0:
+        os.environ['CUDA_VISIBLE_DEVICES']="-1"
+    else:
+        if isinstance(gpuid, list):
+            os.environ['CUDA_VISIBLE_DEVICES'] = ",".join(str(i - 1) for i in gpuid)
+        else:
+            os.environ['CUDA_VISIBLE_DEVICES'] = str(gpuid - 1)
+        logger.debug(' - set CUDA_VISIBLE_DEVICES= %s' % os.environ['CUDA_VISIBLE_DEVICES'])
+
+import numpy as np
+import faiss
+
+LASER = '/opt/LASER'
+os.environ['LASER'] = LASER
+sys.path.append(LASER + '/source')
+sys.path.append(LASER + '/source/lib')
+
+from text_processing import Token, BPEfastApply
+from embed import SentenceEncoder, EncodeLoad, EncodeFile, EmbedLoad
+from mine_bitexts import TextLoadUnify, knn, score, score_candidates
+
+def tok(lang, inputF, outputF, verbose):
+    Token(inputF,
+          outputF,
+          lang=lang,
+          romanize=True if lang == 'el' else False,
+          lower_case=True,
+          gzip=False,
+          verbose=verbose,
+          over_write=False)
+
+def bpe(bpecodes, inputF, outputF, verbose):
+    BPEfastApply(inputF,
+                 outputF,
+                 bpecodes,
+                 verbose=verbose,
+                 over_write=False)
+
+
+def emb(encoder, inputF, outputF, verbose, buffer_size = 10000):
+    EncodeFile(encoder,
+               inputF,
+               outputF,
+               verbose=verbose,
+               over_write=False,
+               buffer_size=buffer_size)
+
+def TokBpeEmb(lang, inputF, tokF, bpeF, embF, bpeCodesF, encoder, verbose, gpuid=None):
+    tok(lang, inputF, tokF, verbose)
+    bpe(bpeCodesF, tokF, bpeF, verbose)
+    if gpuid is not None:
+        setCUDA_VISIBLE_DEVICES(gpuid)
+
+    if isinstance(encoder, str):
+        encoder = loadEncoder(encoder)
+
+    emb(encoder, bpeF, embF, verbose)
+
+
+def loadEncoder(encoderF, buffer_size = 10000, max_tokens = 12000, max_sentences = None, cpu = False, stable = False):
+    buffer_size = max(buffer_size, 1)
+    assert not max_sentences or max_sentences <= buffer_size, '--max-sentences/--batch-size cannot be larger than --buffer-size'
+
+    logger.info(' - Encoder: loading {}'.format(encoderF))
+    return SentenceEncoder(encoderF,
+                           max_sentences=max_sentences,
+                           max_tokens=max_tokens,
+                           sort_kind='mergesort' if stable else 'quicksort',
+                           cpu=cpu)
+
+
+def unique_embeddings(emb, ind):
+    aux = {j: i for i, j in enumerate(ind)}
+    logger.info(' - unify embeddings: {:d} -> {:d}'.format(len(emb), len(aux)))
+    return emb[[aux[i] for i in range(len(aux))]]
+
+
+def scoreBitext(src_inds, trg_inds, x, y, x2y_mean, y2x_mean, outputF, encoding, margin):
+    logger.info(' - Scoring parallel data')
+    fout = open(outputF, mode='w', encoding=encoding, errors='surrogateescape')
+    for i, j in zip(src_inds, trg_inds):
+        print(score(x[i], y[j], x2y_mean[i], y2x_mean[j], margin), file=fout)
+    fout.close()
+
+def mineBitext(src_sents, trg_sents, x, y, x2y_ind, x2y_mean, y2x_ind, y2x_mean, outputF, margin, retrieval):
+    logger.info(' - mining for parallel data')
+    fwd_scores = score_candidates(x, y, x2y_ind, x2y_mean, y2x_mean, margin, args.verbose)
+    bwd_scores = score_candidates(y, x, y2x_ind, y2x_mean, x2y_mean, margin, args.verbose)
+    fwd_best = x2y_ind[np.arange(x.shape[0]), fwd_scores.argmax(axis=1)]
+    bwd_best = y2x_ind[np.arange(y.shape[0]), bwd_scores.argmax(axis=1)]
+
+    logger.info(' - writing alignments to {:s}'.format(outputF))
+    if args.threshold > 0:
+        logger.info(' - with threshold of {:f}'.format(args.threshold))
+
+
+    fout = open(outputF, mode='w', encoding=args.encoding, errors='surrogateescape')
+
+    if retrieval == 'fwd':
+        for i, j in enumerate(fwd_best):
+            print(fwd_scores[i].max(), src_sents[i], trg_sents[j], sep='\t', file=fout)
+    if retrieval == 'bwd':
+        for j, i in enumerate(bwd_best):
+            print(bwd_scores[j].max(), src_sents[i], trg_sents[j], sep='\t', file=fout)
+    if retrieval == 'intersect':
+        for i, j in enumerate(fwd_best):
+            if bwd_best[j] == i:
+                print(fwd_scores[i].max(), src_sents[i], trg_sents[j], sep='\t', file=fout)
+    if retrieval == 'max':
+        indices = np.stack((np.concatenate((np.arange(x.shape[0]), bwd_best)),
+                            np.concatenate((fwd_best, np.arange(y.shape[0])))), axis=1)
+        scores = np.concatenate((fwd_scores.max(axis=1), bwd_scores.max(axis=1)))
+        seen_src, seen_trg = set(), set()
+        for i in np.argsort(-scores):
+            src_ind, trg_ind = indices[i]
+            if not src_ind in seen_src and not trg_ind in seen_trg:
+                seen_src.add(src_ind)
+                seen_trg.add(trg_ind)
+                if scores[i] > args.threshold:
+                    print(scores[i], src_sents[src_ind], trg_sents[trg_ind], sep='\t', file=fout)
+    fout.close()
 
 
 class TuminerUtility(Utility):
@@ -45,8 +167,8 @@ class TuminerUtility(Utility):
 
     def exec_function(self, args):
 
-        LASER = '/opt/LASER'
-        os.environ['LASER'] = LASER
+        setCUDA_VISIBLE_DEVICES(args.gpuid)
+
         bpeCodesF_local = LASER + '/models/93langs.fcodes'
         encoderF_local = LASER + '/models/bilstm.93langs.2018-12-26.pt'
 
@@ -82,108 +204,6 @@ class TuminerUtility(Utility):
         #################
         # Perform tasks
         #################
-        import numpy as np
-        import faiss
-
-        sys.path.append(LASER + '/source')
-        sys.path.append(LASER + '/source/lib')
-        
-        from text_processing import Token, BPEfastApply
-        from embed import SentenceEncoder, EncodeLoad, EncodeFile, EmbedLoad
-        from mine_bitexts import TextLoadUnify, knn, score, score_candidates
-        
-        def loadEncoder(encoderF, buffer_size = 10000, max_tokens = 12000, max_sentences = None, cpu = False, stable = False):
-            buffer_size = max(buffer_size, 1)
-            assert not max_sentences or max_sentences <= buffer_size, '--max-sentences/--batch-size cannot be larger than --buffer-size'
-        
-            logger.info(' - Encoder: loading {}'.format(encoderF))
-            return SentenceEncoder(encoderF,
-                                   max_sentences=max_sentences,
-                                   max_tokens=max_tokens,
-                                   sort_kind='mergesort' if stable else 'quicksort',
-                                   cpu=cpu)
-        
-        def tok(lang, inputF, outputF):
-            Token(inputF,
-                  outputF,
-                  lang=lang,
-                  romanize=True if lang == 'el' else False,
-                  lower_case=True,
-                  gzip=False,
-                  verbose=args.verbose,
-                  over_write=False)
-        
-        def bpe(bpecodes, inputF, outputF):
-            BPEfastApply(inputF,
-                         outputF,
-                         bpecodes,
-                         verbose=args.verbose,
-                         over_write=False)
-        
-        
-        def emb(encoder, inputF, outputF, buffer_size = 10000):
-            EncodeFile(encoder,
-                       inputF,
-                       outputF,
-                       verbose=args.verbose,
-                       over_write=False,
-                       buffer_size=buffer_size) 
-        
-        def unique_embeddings(emb, ind):
-            aux = {j: i for i, j in enumerate(ind)}
-            logger.info(' - unify embeddings: {:d} -> {:d}'.format(len(emb), len(aux)))
-            return emb[[aux[i] for i in range(len(aux))]]
-        
-        def scoreBitext(src_inds, trg_inds, x, y, x2y_mean, y2x_mean, outputF, encoding, margin):
-
-            logger.info(' - Scoring parallel data')
-            
-            fout = open(outputF, mode='w', encoding=encoding, errors='surrogateescape')
-            for i, j in zip(src_inds, trg_inds):
-                print(score(x[i], y[j], x2y_mean[i], y2x_mean[j], margin), file=fout)        
-            fout.close()
-
-        def mineBitext(src_sents, trg_sents, x, y, x2y_ind, x2y_mean, y2x_ind, y2x_mean, outputF, margin, retrieval):
-
-            logger.info(' - mining for parallel data')
-
-            fwd_scores = score_candidates(x, y, x2y_ind, x2y_mean, y2x_mean, margin, args.verbose)
-            bwd_scores = score_candidates(y, x, y2x_ind, y2x_mean, x2y_mean, margin, args.verbose)
-            fwd_best = x2y_ind[np.arange(x.shape[0]), fwd_scores.argmax(axis=1)]
-            bwd_best = y2x_ind[np.arange(y.shape[0]), bwd_scores.argmax(axis=1)]
-            
-            logger.info(' - writing alignments to {:s}'.format(outputF))
-            if args.threshold > 0:
-                logger.info(' - with threshold of {:f}'.format(args.threshold))
-
-
-            fout = open(outputF, mode='w', encoding=args.encoding, errors='surrogateescape')
-
-            if retrieval == 'fwd':
-                for i, j in enumerate(fwd_best):
-                    print(fwd_scores[i].max(), src_sents[i], trg_sents[j], sep='\t', file=fout)
-            if retrieval == 'bwd':
-                for j, i in enumerate(bwd_best):
-                    print(bwd_scores[j].max(), src_sents[i], trg_sents[j], sep='\t', file=fout)
-            if retrieval == 'intersect':
-                for i, j in enumerate(fwd_best):
-                    if bwd_best[j] == i:
-                        print(fwd_scores[i].max(), src_sents[i], trg_sents[j], sep='\t', file=fout)
-            if retrieval == 'max':
-                indices = np.stack((np.concatenate((np.arange(x.shape[0]), bwd_best)),
-                                    np.concatenate((fwd_best, np.arange(y.shape[0])))), axis=1)
-                scores = np.concatenate((fwd_scores.max(axis=1), bwd_scores.max(axis=1)))
-                seen_src, seen_trg = set(), set()
-                for i in np.argsort(-scores):
-                    src_ind, trg_ind = indices[i]
-                    if not src_ind in seen_src and not trg_ind in seen_trg:
-                        seen_src.add(src_ind)
-                        seen_trg.add(trg_ind)
-                        if scores[i] > args.threshold:
-                            print(scores[i], src_sents[src_ind], trg_sents[trg_ind], sep='\t', file=fout)
-            fout.close()
-
-        encoder = loadEncoder(encoderF_local) 
         with tempfile.TemporaryDirectory() as tmpdir:
             srcTokF = os.path.join(tmpdir, 'srctok')
             srcBpeF = os.path.join(tmpdir, 'srcbpe')
@@ -193,15 +213,30 @@ class TuminerUtility(Utility):
             tgtBpeF = os.path.join(tmpdir, 'tgtbpe')
             tgtEmbF = os.path.join(tmpdir, 'tgtemb')
 
-            tok(args.srclang, srcF_local, srcTokF)
-            bpe(bpeCodesF_local, srcTokF, srcBpeF)
-            emb(encoder, srcBpeF, srcEmbF)
+            logger.debug(' - gpuid: %s' % args.gpuid)
 
-            tok(args.tgtlang, tgtF_local, tgtTokF)
-            bpe(bpeCodesF_local, tgtTokF, tgtBpeF)
-            emb(encoder, tgtBpeF, tgtEmbF)
+            if isinstance(args.gpuid, list):
+                logger.debug(' - perform src and tgt embedding in parallel')
+
+                import torch.multiprocessing as mp
+
+                srcP = mp.Process(target=TokBpeEmb, args=(args.srclang, srcF_local, srcTokF, srcBpeF, srcEmbF, bpeCodesF_local, encoderF_local, args.verbose, args.gpuid[0]))
+                srcP.start()
+
+                tgtP = mp.Process(target=TokBpeEmb, args=(args.tgtlang, tgtF_local, tgtTokF, tgtBpeF, tgtEmbF, bpeCodesF_local, encoderF_local, args.verbose, args.gpuid[1]))
+                tgtP.start()
+
+                srcP.join()
+                tgtP.join()
+
+            else:
+                logger.debug(' - perform src and tgt embedding in series')
+                encoder = loadEncoder(encoderF_local)
+                TokBpeEmb(args.srclang, srcF_local, srcTokF, srcBpeF, srcEmbF, bpeCodesF_local, encoder, args.verbose, args.gpuid)
+                TokBpeEmb(args.tgtlang, tgtF_local, tgtTokF, tgtBpeF, tgtEmbF, bpeCodesF_local, encoder, args.verbose, args.gpuid)
 
             #LASER options
+            setCUDA_VISIBLE_DEVICES(args.gpuid)
             unify, retrieval, margin, neighborhood, gpu = True, 'max', 'ratio', 5, (args.gpuid is not 0)
 
             args.unify = unify
@@ -228,7 +263,7 @@ class TuminerUtility(Utility):
                 logger.info(' - perform {:d}-nn target against source'.format(neighborhood))
                 y2x_sim, y2x_ind = knn(y, x, min(x.shape[0], neighborhood), gpu)
                 y2x_mean = y2x_sim.mean(axis=1)
-        
+
             # margin function
             if margin == 'absolute':
                 margin = lambda a, b: a
