11409a16297b330366e7250e9ea92fe99ce1160a
Dakun Zhang <dakun.zhang@systrangroup.com>
2019-08-30 17:54:38 +0200

    reformulate arguments

--- utilities/similarity/entrypoint.py
+++ utilities/similarity/entrypoint.py
@@ -22,51 +22,176 @@ class SimilarityUtility(Utility):
     def name(self):
         return "similarity"
 
+    def declare_arguments(self, parser):
+        subparsers_similarity = parser.add_subparsers(help='Run type', dest='cmd', metavar='{simtrain,simapply}')
+        parser_similarity = subparsers_similarity.add_parser('similarity',
+                            help='Train or apply similarity model.')
+        parser_similarity.add_argument('-mdir', required=True,
+                            help='directory to save/restore models')
+        parser_similarity.add_argument('-batch_size', required=False, type=int, default=32,
+                            help='number of examples per batch [32]')
+        parser_similarity.add_argument('-seed', required=False, type=int, default=1234,
+                            help='seed for randomness [1234]')
+        parser_similarity.add_argument('-debug', action='store_true',
+                            help='debug mode')
+
+        parser_train = subparsers_similarity.add_parser('simtrain', parents=[parser_similarity],
+                            add_help=False,
+                            help='similarity module train mode')
+        parser_train.add_argument('-trn', required=True, help='training data')
+        parser_train.add_argument('-seq_size', required=False, type=int, default=50,
+                            help='sentences larger than this number of src/tgt words are filtered out [50]')
+        parser_train.add_argument('-dev', required=False,
+                            help='validation data')
+        parser_train.add_argument('-src_tok', required=False,
+                            help='if provided, json tokenization options for onmt tokenization, points to vocabulary file')
+        parser_train.add_argument('-src_voc', required=False,
+                            help='vocabulary of src words (needed to initialize learning)')
+        parser_train.add_argument('-tgt_tok', required=False,
+                            help='if provided, json tokenization options for onmt tokenization, points to vocabulary file')
+        parser_train.add_argument('-tgt_voc', required=False,
+                            help='vocabulary of tgt words (needed to initialize learning)')
+        parser_train.add_argument('-src_emb', required=False,
+                            help='embeddings of src words (needed to initialize learning)')
+        parser_train.add_argument('-tgt_emb', required=False,
+                            help='embeddings of tgt words (needed to initialize learning)')
+        parser_train.add_argument('-src_emb_size', required=False, type=int,
+                            help='size of src embeddings if -src_emb not used')
+        parser_train.add_argument('-tgt_emb_size', required=False,
+                            help='size of tgt embeddings if -tgt_emb not used')
+        parser_train.add_argument('-src_lstm_size', required=False, type=int, default=256,
+                            help='hidden units for src bi-lstm [256]')
+        parser_train.add_argument('-tgt_lstm_size', required=False, type=int, default=256,
+                            help='hidden units for tgt bi-lstm [256]')
+        parser_train.add_argument('-lr', required=False, type=float, default=1.0,
+                            help='initial learning rate [1.0]')
+        parser_train.add_argument('-lr_decay', required=False, type=float, default=0.9,
+                            help='learning rate decay [0.9]')
+        parser_train.add_argument('-lr_method', required=False, default='adagrad',
+                            help='GD method either: adam, adagrad, adadelta, sgd, rmsprop [adagrad]')
+        parser_train.add_argument('-aggr', required=False, default='lse',
+                            help='aggregation operation: sum, max, lse [lse]')
+        parser_train.add_argument('-r', required=False, type=float, default=1.0,
+                            help='r for lse [1.0]')
+        parser_train.add_argument('-dropout', required=False, type=float, default=0.3,
+                            help='dropout ratio [0.3]')
+        parser_train.add_argument('-mode', required=False, default='alignment',
+                            help='mode (alignment, sentence) [alignment]')
+        parser_train.add_argument('-max_sents', required=False, type=int, default=0,
+                            help='Consider this number of sentences per batch (0 for all) [0]')
+        parser_train.add_argument('-n_epochs', required=False, type=int, default=1,
+                            help='train for this number of epochs [1]')
+        parser_train.add_argument('-report_every', required=False, type=int, default=1000,
+                            help='report every this many batches [1000]')
+
+        parser_apply = subparsers_similarity.add_parser('simapply', parents=[parser_similarity],
+                            add_help=False,
+                            help='similarity module apply mode')
+        parser_apply.add_argument('-tst', required=True, help='testing data')
+        parser_apply.add_argument('-epoch', required=False, type=int,
+                            help='epoch to use ([mdir]/epoch[epoch] must exist, by default the latest one in mdir)')
+        parser_apply.add_argument('-output', required=False, default='-',
+                            help='output file [- by default is STDOUT]')
+        parser_apply.add_argument('-q', required=False, action='store_true',
+                            help='quiet mode, just output similarity score')
+        parser_apply.add_argument('-show_matrix', required=False, action='store_true',
+                            help='output formatted alignment matrix (mode must be alignment)')
+        parser_apply.add_argument('-show_svg', required=False, action='store_true',
+                            help='output alignment matrix using svg-like html format (mode must be alignment)')
+        parser_apply.add_argument('-show_align', required=False, action='store_true',
+                            help='output source/target alignment matrix (mode must be alignment)')
+        parser_apply.add_argument('-show_last', required=False, action='store_true',
+                            help='output source/target last vectors')
+        parser_apply.add_argument('-show_aggr', required=False, action='store_true',
+                            help='output source/target aggr vectors')
+
     def exec_function(self, args):
-        assert args and (args[0] == "train" or args[0] == "apply"), "invalid parameters for similarity module"
-        if args[0] == "train":
-            assert "-trn" in args, "missing `-trn` parameters in training mode"
-        else:
-            assert "-tst" in args and "-mdir" in args, "missing `-tst` or `-mdir` parameters in inference mode"
-            assert "-output" in args, "missing `-output` parameter in inference mode"
         new_args = []
-        file_parameters = ['-trn', '-dev', '-src_tok', '-tgt_tok', '-src_emb', '-tgt_emb', '-tst', '-mdir']
-        idx = 1
-        output = None
         local_output = None
-        while idx < len(args):
-            val = args[idx]
-            val = resolve_environment_variables(val)
-            new_args.append(val)
-            if val in file_parameters:
-                assert idx+1 < len(args), "missing value for parameters `%s`" % val
-                nextval = args[idx+1]
-                inputs = nextval.split(',')
-                local_inputs = []
-                for the_input in inputs:
-                    local_input = os.path.join(self._data_dir, self._storage.split(the_input)[-1])
-                    print "--", the_input, local_input
-                    if val == '-mdir':
-                        self._storage.get_directory(the_input, local_input)
-                    else:
-                        self._storage.get_file(the_input, local_input)
-                    local_inputs.append(local_input)
-                new_args.append(','.join(local_inputs))
-                idx += 1
-            elif val == '-output':
-                assert idx+1 < len(args), "missing value for parameters `%s`" % val
-                output = args[idx+1]
-                if output == '-':
-                    new_args.append(output)
-                else:
-                    local_output = os.path.join(self._data_dir, self._storage.split(output)[-1])
-                    new_args.append(local_output)
-                idx += 1
-            idx += 1
-        print new_args
+
+        new_args.append('-mdir')
+        new_args.append(self.convert_to_local_file([args.mdir], is_dir=True)[0])
+        new_args.append('-batch_size')
+        new_args.append(str(args.batch_size))
+        new_args.append('-seed')
+        new_args.append(str(args.seed))
+        if args.debug:
+            new_args.append('-debug')
+        if args.cmd == 'simtrain':
+            new_args.append('-trn')
+            new_args.append(self.convert_to_local_file([args.trn])[0])
+            new_args.append('-dev')
+            new_args.append(self.convert_to_local_file([args.dev])[0])
+            new_args.append('-src_tok')
+            new_args.append(self.convert_to_local_file([args.src_tok])[0])
+            new_args.append('-src_voc')
+            new_args.append(self.convert_to_local_file([args.src_voc])[0])
+            new_args.append('-tgt_tok')
+            new_args.append(self.convert_to_local_file([args.tgt_tok])[0])
+            new_args.append('-tgt_voc')
+            new_args.append(self.convert_to_local_file([args.tgt_voc])[0])
+            new_args.append('-src_emb')
+            new_args.append(self.convert_to_local_file([args.src_emb])[0])
+            new_args.append('-tgt_emb')
+            new_args.append(self.convert_to_local_file([args.tgt_emb])[0])
+            new_args.append('-src_emb_size')
+            new_args.append(str(args.src_emb_size))
+            new_args.append('-tgt_emb_size')
+            new_args.append(str(args.tgt_emb_size))
+            new_args.append('-src_lstm_size')
+            new_args.append(str(args.src_lstm_size))
+            new_args.append('-tgt_lstm_size')
+            new_args.append(str(args.tgt_lstm_size))
+            new_args.append('-lr')
+            new_args.append(str(args.lr))
+            new_args.append('-lr_decay')
+            new_args.append(str(args.lr_decay))
+            new_args.append('-lr_method')
+            new_args.append(args.lr_method)
+            new_args.append('-aggr')
+            new_args.append(args.aggr)
+            new_args.append('-r')
+            new_args.append(str(args.r))
+            new_args.append('-dropout')
+            new_args.append(str(args.dropout))
+            new_args.append('-mode')
+            new_args.append(args.mode)
+            new_args.append('-max_sents')
+            new_args.append(str(args.max_sents))
+            new_args.append('-n_epochs')
+            new_args.append(str(args.n_epochs))
+            new_args.append('-report_every')
+            new_args.append(str(args.report_every))
+        if args.cmd == 'simapply':
+            new_args.append('-tst')
+            new_args.append(self.convert_to_local_file([args.tst])[0])
+            if args.epoch:
+                new_args.append('-epoch')
+                new_args.append(str(args.epoch))
+            new_args.append('-output')
+            if args.output == '-':
+                new_args.append('-')
+            else:
+                local_output = self.convert_to_local_file([args.output])[0]
+                new_args.append(local_output)
+            if args.q:
+                new_args.append('-q')
+            if args.show_matrix:
+                new_args.append('-show_matrix')
+            if args.show_svg:
+                new_args.append('-show_svg')
+            if args.show_align:
+                new_args.append('-show_align')
+            if args.show_last:
+                new_args.append('-show_last')
+            if args.show_aggr:
+                new_args.append('-show_aggr')
+
+        logger.info("command line option: %s" % " ".join(new_args))
         main(['similarity.py'] + new_args)
+
         if local_output is not None:
-            self._storage.push(local_output, output)
+            self._storage.push(local_output, args.output)
 
 
 if __name__ == '__main__':
