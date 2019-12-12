55bf2cd1054a86879a29f09dbfa5cbe14de796c4
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-12-12 15:16:58 +0100

    Fix file encoding in paste_files function

---
+++ test/test_data.py
@@ -0,0 +1,15 @@
+import pytest
+
+from nmtwizard import data
+
+def test_paste_files(tmpdir):
+    a = str(tmpdir.join("a.txt"))
+    b = str(tmpdir.join("b.txt"))
+    c = str(tmpdir.join("c.txt"))
+    with open(a, "w") as af:
+        af.write("1 2 3\n4 5\n")
+    with open(b, "w") as bf:
+        bf.write("7 8\n9\n")
+    data.paste_files([a, b], c, separator="|")
+    with open(c) as c:
+        assert c.read() == "1 2 3|7 8\n4 5|9\n"
