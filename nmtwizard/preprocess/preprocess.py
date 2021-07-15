a1b1ac9c2a1d55ec297d88735ee588631ce00871
Guillaume Klein <guillaume.klein@systrangroup.com>
2021-07-15 17:39:18 +0200

    Allow consuming processed batches in arbitrary order

--- nmtwizard/preprocess/preprocess.py
+++ nmtwizard/preprocess/preprocess.py
@@ -2,6 +2,7 @@
 
 import copy
 import collections
+import functools
 import multiprocessing
 import multiprocessing.managers
 import os
@@ -60,10 +61,10 @@ def _process_batch(
     config=None,
     process_type=None,
     exit_step=None,
-    override_label=None,
     shared_state=None,
 ):
     """Rebuilds the pipeline if required and processes a batch of TUs."""
+    override_label = _get_corpus_label(tu_batch)
     if pipeline is None or override_label != pipeline.override_label:
         if override_label is None:
             logger.info("Building default processing pipeline")
@@ -97,18 +98,16 @@ worker_pipeline = None
 
 
 def _process_batch_on_worker(
-    tu_batch,
+    inputs,
     options=None,
-    # Arguments below are used to rebuild the pipeline, if required.
     config=None,
     process_type=None,
     exit_step=None,
-    override_label=None,
-    shared_state=None,
 ):
     """Processes a batch of TUs using the pipeline cached on the worker process."""
     global worker_pipeline
     try:
+        tu_batch, shared_state = inputs
         outputs, worker_pipeline = _process_batch(
             worker_pipeline,
             tu_batch,
@@ -116,7 +115,6 @@ def _process_batch_on_worker(
             config=config,
             process_type=process_type,
             exit_step=exit_step,
-            override_label=override_label,
             shared_state=shared_state,
         )
     except Exception as e:
@@ -170,7 +168,6 @@ class Processor(object):
                     config=self._config,
                     process_type=self._pipeline_type,
                     exit_step=preprocess_exit_step,
-                    override_label=override_label,
                     shared_state=shared_state,
                 )
                 consumer(outputs)
@@ -178,45 +175,28 @@ class Processor(object):
         else:
             logger.info("Start processing using %d worker(s)", self._num_workers)
 
+            def _get_iterator():
+                for tu_batch in loader():
+                    override_label = _get_corpus_label(tu_batch)
+                    shared_state = self._global_shared_state.get(override_label)
+                    yield tu_batch, shared_state
+
+            process_func = functools.partial(
+                _process_batch_on_worker,
+                options=options,
+                config=self._config,
+                process_type=self._pipeline_type,
+                exit_step=preprocess_exit_step,
+            )
+
             # Because of the Python GIL (Global Interpreter Lock), we need to use
             # process-based workers to enable true parallelism. The downside is
             # that it duplicates resources for each worker, increasing the
             # memory usage. This is mitigated by the better stream processing of
             # the loader/consumer which avoids loading the full corpus in memory.
             with multiprocessing.Pool(processes=self._num_workers) as pool:
-                results = collections.deque()
-
-                for tu_batch in loader():
-                    override_label = _get_corpus_label(tu_batch)
-                    shared_state = self._global_shared_state.get(override_label)
-
-                    # Push the batch in the process queue and get a handle on the result.
-                    results.append(
-                        pool.apply_async(
-                            _process_batch_on_worker,
-                            args=(tu_batch,),
-                            kwds=dict(
-                                options=options,
-                                config=self._config,
-                                process_type=self._pipeline_type,
-                                exit_step=preprocess_exit_step,
-                                override_label=override_label,
-                                shared_state=shared_state,
-                            ),
-                        )
-                    )
-
-                    # Limit the queue max size to avoid loading too many batches in advance.
-                    if len(results) == 2 * self._num_workers:
-                        results[0].wait()
-
-                    # Consume batches that are ready.
-                    while len(results) > 0 and results[0].ready():
-                        consumer(results.popleft().get())
-
-                # Wait and consume all remaining batches.
-                while len(results) > 0:
-                    consumer(results.popleft().get())
+                for result in pool.imap_unordered(process_func, _get_iterator()):
+                    consumer(result)
 
 
 class TrainingProcessor(Processor):
