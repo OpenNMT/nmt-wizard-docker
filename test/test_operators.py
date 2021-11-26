a45e5e22f0d2403932bb37358182fa0103413c7c
Natalia Segal <natalia.segal@systrangroup.com>
2021-11-26 14:45:00 +0100

    First word noise implementation

--- test/test_operators.py
+++ test/test_operators.py
@@ -172,6 +172,9 @@ def test_tokenization_with_non_iso_639_lang():
                 "｟mrk_noisy｠ hell o.",
             ],
         ),
+        (dict(duplicate_word_prob=1), True, "hello.", ["hello hello.."]),
+        (dict(swap_word_prob=1), True, "hello.", [". hello"]),
+        (dict(substitute_word_prob=1, word_embedding_file="/nfs/SSALING-DATA/segal/dev/systran-docker/nmt-wizard-docker/test/corpus/resources/embeddings/dbpedia.ftz"), True, "hello.", ['translator.', 'dichotomy.', 'violin.', 'clarinetist.', 'luce.']),
     ],
 )
 def test_noise(config, training, text, expected):
